91eb725e411862c449e1d26b8f84079a9c5af99b
night <chandankumard.185@gmail.com>
2021-12-05 19:19:26 +0530

    Message Board Initial Commit

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+#NEW
+from django.views.generic import ListView
+from .models import Post
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name='all_posts_list'                #renamed
